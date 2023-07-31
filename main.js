1e11cd39adf9e79f9f18d55add2f94502dff5ced
badr <bernardoantoniod@gmail.com>
2023-07-31 16:48:46 -0400

    foxy iV

--- main.js
+++ main.js
@@ -4,18 +4,27 @@ import { resetSilyObserver } from "./lazy.js";
 
 const FOXYS_API = "https://randomfox.ca/floof/";
 
-
 async function gettingFoxys () {
-    const response = await fetch(FOXYS_API);
-    const data = await response.json();
-    console.log("These is the data:")
-    console.log(data);
-    return data.image;
+    try {
+        const response = await fetch(FOXYS_API);
+        console.log(`Response Status: ${response.status}`);
+        if (response.status != 200) {
+            throw new Error (`Error fetching GettingFoxys: ${statusName}`);
+        }
+        const data = await response.json();
+        console.log("These is the data:")
+        console.log(data);
+        return data.image;
+
+    } catch (error) {
+        const errorGettingFoxys = document.querySelector(".error-span");
+        errorGettingFoxys.textContent = `Error: ${error.message}`;
+        throw new Error ("There was an error getting foxys");
+    };
+
 }
 
-
 async function settingFoxy () {
-
     const articleContainer = document.createElement("article");
     articleContainer.className = "main-container-article";
     const foxyImage = document.createElement("img");
@@ -24,9 +33,7 @@ async function settingFoxy () {
     foxyImage.dataset.src = await gettingFoxys();
     articleContainer.appendChild(foxyImage);
     return articleContainer;
-
 }
-
 
 const foxysMainContainer = document.querySelector(".main-container");
 foxysMainContainer.className = "main-container";
@@ -36,10 +43,8 @@ async function postFoxy () {
     registerImages(newFoxy);
 }
 
-
 const postingAFoxyButton = document.querySelector("#get-fox-button");
 postingAFoxyButton.addEventListener("click", postFoxy); 
-
 
 const deletingAllFoxys = document.querySelector("#delete-all-fox-button");
 deletingAllFoxys.addEventListener("click", () => {
